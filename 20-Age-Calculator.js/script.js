b56e739a2ee3d901056774ff1e8e6ebb9bee7a01
adityadomle <adityadomle14@gmail.com>
2025-05-31 09:47:09 +0530

    fix buges

--- 20-Age-Calculator.js/script.js
+++ 20-Age-Calculator.js/script.js
@@ -1,45 +1,45 @@
-function calculateAge(){
+function calculateAge() {
 
-    const today = new Date();
-    const birthdateInput = document.getElementById("birthdate").value;
-    const birthdateParts = birthdateInput.split("-");   
-    const birthDay = birthdateParts[0];
-    const birthMonth = birthdateParts[1] - 1; 
-    const birthYear = birthdateParts[2];
-   
-    const birthDate = new Date(birthYear,birthMonth,birthDay);
+  const today = new Date();
+  const birthdateInput = document.getElementById("birthdate").value;
+  const birthdateParts = birthdateInput.split("-");
+  const birthDay = birthdateParts[0];
+  const birthMonth = birthdateParts[1] - 1;
+  const birthYear = birthdateParts[2];
 
-    console.log(birthdateInput);
-    console.log(birthdateParts);
-    console.log(birthDay);
-    console.log(birthMonth);
-    console.log(birthYear);
+  const birthDate = new Date(birthYear, birthMonth, birthDay);
 
-    const isValidDate = (date) =>{
-    return(
-        Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)  
+  console.log(birthdateInput);
+  console.log(birthdateParts);
+  console.log(birthDay);
+  console.log(birthMonth);
+  console.log(birthYear);
+
+  const isValidDate = (date) => {
+    return (
+      Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)
     );
 
-    };
+  };
 
-    if(!isValidDate(birthDate)){
+  if (!isValidDate(birthDate)) {
     alert("Invalid Date Format: Please Enter a valid date in DD-MM-YYYY format.");
     return;
-    }
+  }
 
-   const ageInMilliseconds = today - birthDate;  
-   const ageInseconds = Math.floor(ageInMilliseconds/1000);
-   const ageInMinutes = Math.floor(ageInseconds/60);
-   const ageInHours= Math.floor(ageInMinutes/60);
-   const ageInDays= Math.floor(ageInHours/24);
-   const ageInWeeks= Math.floor(ageInDays/7);
-   const ageInMonths= Math.floor(ageInDays/30.436875);
-   const ageInYears= Math.floor(ageInDays/365.25);
+  const ageInMilliseconds = today - birthDate;
+  const ageInseconds = Math.floor(ageInMilliseconds / 1000);
+  const ageInMinutes = Math.floor(ageInseconds / 60);
+  const ageInHours = Math.floor(ageInMinutes / 60);
+  const ageInDays = Math.floor(ageInHours / 24);
+  const ageInWeeks = Math.floor(ageInDays / 7);
+  const ageInMonths = Math.floor(ageInDays / 30.436875);
+  const ageInYears = Math.floor(ageInDays / 365.25);
 
-   const resultContainer = document.getElementById("resultContainer");
-   const result = document.getElementById("result");
+  const resultContainer = document.getElementById("resultContainer");
+  const result = document.getElementById("result");
 
-   result.innerHTML = `
+  result.innerHTML = `
       <div class="result-item">
         <h3>Age:</h3>
         <p>${ageInYears} Years ${ageInMonths % 12} Months ${ageInDays % 30} Days</p>
@@ -71,7 +71,7 @@ function calculateAge(){
    `;
 
 
-   resultContainer.style.display = "block";
+  resultContainer.style.display = "block";
 
 }
 
@@ -81,8 +81,8 @@ function calculateAge(){
 
 
 const ageCalculatorForm = document.getElementById("ageCalculator");
-ageCalculatorForm.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    calculateAge();
+ageCalculatorForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  calculateAge();
 
 });
